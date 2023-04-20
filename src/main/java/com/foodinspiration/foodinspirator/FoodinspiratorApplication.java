38c6ffb5de3c035377b6b14b2b5651a566315f1f
0xolerdk <olerdikon@gmail.com>
2023-04-20 23:46:08 +0200

    add first commit

---
+++ src/main/java/com/foodinspiration/foodinspirator/FoodinspiratorApplication.java
@@ -0,0 +1,13 @@
+package com.foodinspiration.foodinspirator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodinspiratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodinspiratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodinspiration/foodinspirator/FoodinspiratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodinspiration.foodinspirator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodinspiratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
